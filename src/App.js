e03501fe7c98a00badee27dcd90b82dbe78f2927
AbdullahSharif <abdullah.29.umar@gmail.com>
2023-01-14 00:45:39 +0500

    redux set up completed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Cart from "./pages/cart";
 import Navbar from './components/Navbar';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path='/cart' element={<Cart />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path='/cart' element={<Cart />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+
     </div>
 
 
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -6,7 +6,14 @@ const cartSlice = createSlice({
     name: "cart",        // name of the slice
     initialState,       // provide the initial state
     reducers: {         // define the reducer. it contains the methods which we will use to update the state in the slice.
-        add() { },
-        remove() { }
+        add(state, action) {
+            state.push(action.payload);
+        },
+        remove(state, action) {
+            state = state.filter(item => (item.id !== action.payload));
+        }
     }
 });
+
+export const { add, remove } = cartSlice.actions;
+export default cartSlice.reducer;
